faa83145a89840d77c5248ef0247a2a96aecf941
cauapdebiasi <cauadebiasi@gmail.com>
2026-04-05 00:49:27 -0300

    cria configuração do banco com sequelize

---
+++ backend/src/config/database.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize';
+import { env } from './env.js';
+
+const sequelize = new Sequelize(
+    env.DB_NAME,
+    env.DB_USER,
+    env.DB_PASS,
+    {
+        host: env.DB_HOST,
+        dialect: 'postgres',
+        logging: false,
+    }
+);
+
+export default sequelize;
