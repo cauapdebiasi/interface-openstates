60836fa2b30bc0f8095aaf476715eab3e5973d85
cauapdebiasi <cauadebiasi@gmail.com>
2026-04-05 01:37:08 -0300

    melhora mensagens de validação das variáveis de ambiente

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,19 +5,19 @@ dotenv.config();
 
 const envSchema = z.object({
   PORT: z.string().default('3000'),
-  DB_NAME: z.string().min(1, 'DB_NAME é obrigatório'),
-  DB_USER: z.string().min(1, 'DB_USER é obrigatório'),
-  DB_PASS: z.string().min(1, 'DB_PASS é obrigatório'),
+  DB_NAME: z.string('DB_NAME é obrigatório').min(1, 'DB_NAME deve conter pelo menos um caractere'),
+  DB_USER: z.string('DB_USER é obrigatório').min(1, 'DB_USER deve conter pelo menos um caractere'),
+  DB_PASS: z.string('DB_PASS é obrigatório').min(1, 'DB_PASS deve conter pelo menos um caractere'),
   DB_HOST: z.string().default('localhost'),
   DB_PORT: z.coerce.number().default(5433),
-  OPENSTATES_API_KEY: z.string().min(1, 'OPENSTATES_API_KEY é obrigatório'),
+  OPENSTATES_API_KEY: z.string('OPENSTATES_API_KEY é obrigatório').min(1, 'OPENSTATES_API_KEY deve conter pelo menos um caractere'),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
   console.error('Variáveis de ambiente inválidas!');
-  console.error(z.treeifyError(_env.error));
+  console.error(z.prettifyError(_env.error));
   process.exit(1);
 }
 
